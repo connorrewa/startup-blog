96f833de960eb860b6c53f6837187758c8256364
connorrewa <connorrewa13@hotmail.com>
2024-12-18 22:34:49 -0500

    init assets and style homepage

---
+++ app/(root)/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <>
+            <section className='pink_container'>
+                <h1 className='heading'>
+                    Pitch your startup <br /> Connect with Entrepreneurs
+                </h1>
+                <p className='sub-heading !max-w-3xl'>
+                    Submit Ideas, vote on pitches, and get noticed in
+                    competitions.
+                </p>
+            </section>
+        </>
+    );
+};
+
+export default page;
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,52 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { signIn, signOut, auth } from '@/auth';
+
+const Navbar = async () => {
+    const session = await auth();
+
+    return (
+        <>
+            <header className='px-5 py-3 bg-white shadow-sm text-black'>
+                <nav className='flex justify-between items-center '>
+                    <Link href='/'>logo</Link>
+
+                    <div className='flex items-center gap-5'>
+                        {session && session?.user ? (
+                            <>
+                                <Link href='/startup/create'>
+                                    <span>Create</span>
+                                </Link>
+
+                                <form
+                                    action={async () => {
+                                        'use server';
+                                        await signOut({ redirectTo: '/' });
+                                    }}
+                                >
+                                    <button type='submit'>Logout</button>
+                                </form>
+
+                                <Link href={`/user/${session?.id}`}>
+                                    <span>{session?.user?.name}</span>
+                                </Link>
+                            </>
+                        ) : (
+                            <form
+                                action={async () => {
+                                    'use server';
+                                    await signIn('github');
+                                }}
+                            >
+                                <button type='submit'>login</button>
+                            </form>
+                        )}
+                        ;
+                    </div>
+                </nav>
+            </header>
+        </>
+    );
+};
+
+export default Navbar;
